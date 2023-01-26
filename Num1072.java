d7b31f14bdc772b5094698ce6e0dbd445553c1cc
Humaira Khan <khanhumairakhan000@gmail.com>
2023-01-27 01:50:53 +0600

    Beecrowd

--- Num1072.java
+++ Num1072.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Num1072 {
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,y;
+        n= sc.nextInt();
+        int countin =0;
+        int countout =0;
+        for (int i = 1; i <=n ; i++) {
+            y = sc.nextInt();
+            if(y>=10 && y<=20){
+                countin++;
+            }
+            else countout++;
+
+        }
+        System.out.println(countin+" in");
+        System.out.println(countout+" out");
+    }
 }
--- Num1073.java
+++ Num1073.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Num1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int x;
+        for (int i = 2; i <=n ; i=i+2) {
+
+            x=i*i;
+            System.out.println(i+"^"+2+" = "+x);
+        }
+    }
 }
